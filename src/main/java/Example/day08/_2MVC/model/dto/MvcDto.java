1604351624e3a1f26c886ddabd6dc462fdeb218e
msh-94 <alstjdgh2003@naver.com>
2025-08-11 12:23:25 +0900

    Spring 8일차 MVC패턴 3Tire 세팅

--- src/main/java/Example/day08/_2MVC/model/dto/MvcDto.java
+++ src/main/java/Example/day08/_2MVC/model/dto/MvcDto.java
@@ -2,11 +2,11 @@ package Example.day08._2MVC.model.dto;// 패키지명
 
 import lombok.*;
 
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class MvcDto {// class start
-
+    private String var1;
 }// class end
