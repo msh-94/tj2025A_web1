adf5f4f5777f9dd87e04a6ab0e9ae830bc9043c9
msh-94 <alstjdgh2003@naver.com>
2025-08-11 11:30:23 +0900

    Spring 8일차 IOC 와 DI MVC 패턴

---
+++ src/main/java/Example/day08/_1의존성/Dependency.java
@@ -0,0 +1,59 @@
+package Example.day08._1의존성; // 패키지명
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+// [방법1] 해당 인스턴스를 직접 (new) 생성하여 메소드 호출
+class SampleDao1 { void method1(){} } // class end
+class SampleController1{
+    // 다른 클래스의 메소드 호출방법
+    SampleDao1 sampleDao1 = new SampleDao1();
+    public void method2(){
+        sampleDao1.method1();
+    }// func end
+}// class end
+
+// [방법2] 해당 인스턴스를 미리 (new) 생성하여 메소드 호출 = 싱글톤
+class SampleDao2{
+    private static final SampleDao2 instance = new SampleDao2();
+    private SampleDao2(){}
+    public static SampleDao2 getInstance(){ return instance; }
+    public void method1(){} // func end
+} // class end
+class SampleController2{
+    // 다른 클래스의 메소드 호출방법
+    private final SampleDao2 sampleDao2 = SampleDao2.getInstance();
+    public void method2(){
+        sampleDao2.method1();
+    }// func end
+}// class end
+
+// [방법3] IOC 와 DI
+@Component // * 스프링 컨테이너(저장소)에 빈(객체/인스턴스) 등록 , 스프링 실행 시 *자동*으로 인스턴스 생성
+class SampleDao3{
+    void method1(){}
+}// class end
+    // DI 방법1
+class SampleController3{
+    @Autowired // 스프링 컨테이너(저장소)에 등록된 빈(스프링 컨테이너에 저장된 인스턴스) 꺼내서 멤버변수에 주입
+    private SampleDao3 sampleDao3; // = new SampleDao3();
+    void method2(){ sampleDao3.method1(); }
+}// class end
+
+    // DI 방법2 *Spring 권장
+class SampleController4{
+    private final SampleDao3 sampleDao3; // final(수정불가능키워드,초기값 필수) 사용함으로 불변성(안전) 보장
+    @Autowired // 생성자에 빈 주입 함으로써
+    public SampleController4( SampleDao3 sampleDao3){
+        this.sampleDao3 = sampleDao3;
+    }
+    void method2(){
+        sampleDao3.method1();
+    }
+}// class end
+
+
+public class Dependency { // class start
+
+}// class end
