4caa3b11cb2115d16ab03909299f9772097bacd9
msh-94 <alstjdgh2003@naver.com>
2025-08-18 12:41:23 +0900

    Spring 12일차 카카오지도 사용하기

--- src/main/resources/static/js/kakao/map.js
+++ src/main/resources/static/js/kakao/map.js
@@ -62,4 +62,34 @@ const kakaoMap3 = async() => {
     // 마커에 클릭이벤트를 등록합니다 , { } 마커 클릭시 실행되는 코드
     kakao.maps.event.addListener(marker, 'click', function() { alert(' 해당 마커 클릭 했습니다. ') }); 
 }// func end
-kakaoMap3(); // 예제3
+// kakaoMap3(); // 예제3
+
+// [4] 여러개 마커 표기하기 , https://apis.map.kakao.com/web/sample/multipleMarkerImage/
+const kakaoMap4 = async() => {
+    // 1. 지도를 표시할 div , 지도옵션
+    var mapContainer = document.getElementById('map'),    
+    mapOption = { center: new kakao.maps.LatLng(33.450701, 126.570667), level: 3 };
+    // 2. 지도 생성
+    var map = new kakao.maps.Map(mapContainer, mapOption);     
+    // 3. 마커를 표시할 위치와 title 객체 배열입니다 
+    var positions = [
+        { title: '카카오', latlng: new kakao.maps.LatLng(33.450705, 126.570677) },
+        { title: '생태연못', latlng: new kakao.maps.LatLng(33.450936, 126.569477) },
+        { title: '텃밭', latlng: new kakao.maps.LatLng(33.450879, 126.569940) },
+        { title: '근린공원', latlng: new kakao.maps.LatLng(33.451393, 126.570738 )} ,
+        { title: '부평역', latlng: new kakao.maps.LatLng(37.4905335508 ,  126.7244907601) }];
+    // 5. 마커 이미지의 이미지 주소입니다 // static -> img 폴더내 이미지 경로
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+    // 6. 반복문을 이용한 여러개 마커 생성    
+    for (var i = 0; i < positions.length; i ++) {       
+        var imageSize = new kakao.maps.Size(24, 35); // 마커 이미지의 이미지 크기 입니다          
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); // 마커 이미지를 생성합니다
+        var marker = new kakao.maps.Marker({// 마커를 생성합니다
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지 
+        });
+    }//  for end
+}// func end
+kakaoMap4();
